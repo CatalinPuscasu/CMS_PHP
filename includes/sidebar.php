643df706903ddda755f5561742d45f248dd39662
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-02-11 23:52:46 +0200

    defined the query, also with the if statements and , also amde it search into rows into db

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,11 +6,26 @@
 
                 if (isset($_POST['submit']))  {
 
-                   echo $search = $_POST['search'];
+                 $search = $_POST['search'];
 
                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
 
                    $search_query = mysqli_query($connection, $query);
+
+                   if (!$search_query)  {
+
+                                die("query failed!" . mysqli_error($connection));
+                   }
+
+                   $count = mysqli_num_rows($search_query);
+
+                   if($count == 0) {
+
+                    echo "<h1> NICIUN REZULTAT </h1>";
+                   } else {
+
+                    echo "cv rezultat";
+                   }
 
                 }
 
