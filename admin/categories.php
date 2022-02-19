d87dede55a29874665bc0c0e3bc2733cfbdeba69
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-02-19 22:29:48 +0200

    fixed a little bug

--- admin/categories.php
+++ admin/categories.php
@@ -66,7 +66,7 @@
                  findAllCategories();                
                              ?>
 
-                   //delete query
+                   <!-- //delete query -->
 
            <?php  deleteCategories();?>
            
