67aa6de7e9311c78d5d4f9ac2468b124d707bcf3
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-02-19 22:44:57 +0200

    created the posts admin section, and the table

---
+++ admin/posts.php
@@ -0,0 +1,60 @@
+<?php include "includes/header.php" ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+   <?php include "includes/navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                      <h1 class="page-header">
+                            Bunvenit in sectiunea ADMIN
+                            <small>Subheading</small>
+                        </h1>
+
+                         <table class = "table table-bordered table-hover">
+                             <thead>
+                                 <tr>
+                                     <th>ID</th>
+                                     <th>Author</th>
+                                     <th>Title</th>
+                                     <th>Category</th>
+                                     <th>Status</th>
+                                     <th>Image</th>
+                                     <th>Tags</th>
+                                     <th>Comments</th>
+                                     <th>Date</th>
+                                 </tr>
+                             </thead>
+                             <tbody>
+                                 <tr>
+                                     <td>12</td>
+                                     <td>Eu</td>
+                                     <td>Boostrap</td>
+                                     <td>PHP</td>
+                                     <td>status</td>
+                                     <td>image</td>
+                                     <td>tags</td>
+                                     <td>comments</td>
+                                     <td>date</td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                 
+                </div>
+            </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include "includes/footer.php" ?>
