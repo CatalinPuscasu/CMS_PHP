98f36f9e0c64a241020916ed4a52b0106e9329c4
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-02-20 22:40:07 +0200

    fixes the forms, and added the isset to POST the data

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,3 +1,30 @@
+<?php   
+
+if(isset($_POST['create_post']))  {
+
+   $post_title = $_POST['title'];
+   $post_author = $_POST['author'];
+   $post_category_id = $_POST['post_category_id'];
+   $post_status = $_POST['post_status'];
+
+   $post_image = $_FILES['image']['name'];
+   $post_image_temp = $_FILES['image']['tmp_name'];
+
+   $post_tags = $_POST['post_tags'];
+   $post_content = $_POST['post_content'];
+   $post_date = date('d-m-y');
+   $post_comment_count = 4;
+
+   move_uploaded_file($post_image_temp, "../images/$post_image");
+
+}
+
+
+
+?>
+
+
+
 <form action="" method = "POST" enctype= "multipart/form-data">
 
     <div class="form-group">
@@ -28,7 +55,7 @@
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="file" name="post_tags" id="" class = "form-control">
+        <input type="text" name="post_tags" id="" class = "form-control">
     </div>
 
     <div class="form-group">
