47982dc6bf49b19c36016d25a285e6181f4802da
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-05 00:13:45 +0200

    created the login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -51,6 +51,26 @@
                     <!-- /.input-group -->
                 </div>
 
+                <!-- LOGIN -->
+
+                 <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method = "POST">
+                    <div class="form-group">
+                        <input name = "username" type="text" class="form-control" placeholder="username">                       
+                    </div>
+
+                     <div class="input-group">
+                        <input name = "password" type="password" class="form-control" placeholder="your password">    
+                        <span class="input-group-btn">
+                              <button class="btn btn-primary" name="login" type="submit">Submit!</button>
+                        </span>                   
+                    </div>
+                </form>   
+                <!-- search form -->
+                    <!-- /.input-group -->
+                </div>
+
 
 
 
