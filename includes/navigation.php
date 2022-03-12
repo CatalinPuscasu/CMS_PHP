cc4f1e0eff0d78fed5c535a9610368589f986e59
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-13 00:43:50 +0200

    added the feature for the user to edit the post, once logged

--- includes/navigation.php
+++ includes/navigation.php
@@ -36,12 +36,28 @@
                     <li>
                         <a href="admin">Admin</a>
                     </li>
-                    <!-- <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li> -->
+
+
+                    <?php 
+                    
+                    if (isset($_SESSION['username'])) {
+
+
+                        if (isset($_GET['p_id'])) {
+
+                            $the_post_id = $_GET['p_id'];
+                                
+
+                              echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'> Edit post </a></li>";
+
+                        }
+
+
+                    }
+                    
+                    
+                    ?>
+              
 
 
                 </ul>
