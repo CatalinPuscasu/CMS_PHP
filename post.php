24777ba5cbc7c291b693c00cafbeb588840a96dd
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-15 22:55:04 +0200

    refactored the validation of the comments form, to not allow the user to enter blank comments

--- post.php
+++ post.php
@@ -70,13 +70,15 @@
                  
                  if (isset($_POST['create_comment'])) {
 
-                 $the_post_id = $_GET['p_id'];
+                     $the_post_id = $_GET['p_id'];
 
                  $comment_author = $_POST['comment_author'];
                  $comment_email = $_POST['comment_email'];
                  $comment_content = $_POST['comment_content'];
 
-                 $query = "INSERT INTO comments(comment_post_id, comment_author, comment_content, comment_email, comment_status, comment_date) ";
+                    if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content) ) {
+
+                               $query = "INSERT INTO comments(comment_post_id, comment_author, comment_content, comment_email, comment_status, comment_date) ";
                  $query.="VALUES ($the_post_id, '{$comment_author}', '{$comment_content}', '{$comment_email}', 'unapproved', now()) ";
 
                  $comment_query = mysqli_query($connection, $query);
@@ -87,7 +89,17 @@
                  $update_count = mysqli_query($connection, $comment_count_query);
 
 
+                 } else {
+
+                    echo "<script>alert('Fields cannot be empty!')</script>";
                  }
+
+                        
+                    }
+
+                
+
+          
                  
                  
                  
