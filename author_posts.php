5be588cde047d188e3ef8c08154fed0fff609c81
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-20 23:34:08 +0200

    added the other GET variable to display all the posts related to the author, and removed the hhyperlink on the author name

--- author_posts.php
+++ author_posts.php
@@ -18,9 +18,10 @@
 
 
                 $the_post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
             }
             
-            $query = "SELECT * FROM postari WHERE post_id = $the_post_id ";  // asta e ca sa ne vina doar posatrea la care am dat click pe titlu
+            $query = "SELECT * FROM postari WHERE post_author = '{$the_post_author}' ";  // asta e ca sa ne vina doar posatrea la care am dat click pe titlu
 
             $select_all_posts_query = mysqli_query($connection, $query);
 
@@ -48,7 +49,7 @@
                     <a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
+                    All posts by <?php echo $post_author ?>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
                 <hr>
@@ -109,71 +110,7 @@
 
 
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form" action="" method="POST">
-
-                        <div class="form-group">
-                            <label for="author">Leave your name here</label>
-                            <input type="text" name="comment_author" class="form-control">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">Leave your email here</label>
-                            <input type="email" name="comment_email" class="form-control">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="textarea">Your comment!</label>
-                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-
-            <?php  
-            
-            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-            $query.= "AND comment_status = 'approved' ";
-            $query.= "ORDER BY comment_id DESC ";   // DESC= descending
-            $select_comment_query = mysqli_query($connection, $query);
-
-            if (!$select_comment_query)  {
-                 
-                die('QUERY FAILED' . mysqli_error($connection));
-
-            }
-             while ($row = mysqli_fetch_array($select_comment_query)) {
-                      
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-
-                ?>
-
-                        <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author; ?>
-                            <small><?php echo $comment_date; ?></small>
-                        </h4>
-                        <?php echo $comment_content; ?>
-                    </div>
-                </div>
-
-           <?php  }  ?>
-            
               
-            
-            
         
 
       
