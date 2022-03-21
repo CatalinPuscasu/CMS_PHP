db69e6865865d9d3555262db4f0b3aead7748bab
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-21 23:46:48 +0200

    added the jquery code for the loader

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -29,6 +29,14 @@ $(document).ready(function(){
 
  });
 
+let div_box = "<div id='load-screen'><div id='loading'></div></div>";
+
+$('body').prepend(div_box);
+
+$('#load-screen').delay(700).fadeOut(600, function(){
+   $(this).remove();
+})
+
 });
 
 
