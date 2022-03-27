c820fc15b0cf4367ccfbce694f109779d9bc087b
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-27 17:10:34 +0300

    added the users online link

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -11,6 +11,7 @@
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
+                <li><a href="http://">Users Online : <?php echo usersOnline(); ?></a></li>
                 <li><a href="../index.php">HOME</a></li>
 
                 <li class="dropdown">
