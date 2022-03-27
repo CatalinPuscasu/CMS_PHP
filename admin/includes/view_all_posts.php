69663c54cbe08cf75d881899524a8a87675c177d
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-27 20:44:02 +0300

    added a new query to display the cokment count

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -168,7 +168,15 @@ if(isset($_POST['checkBoxArray'])) {
                           echo "<td>{$post_status}</td>"; 
                           echo "<td><img width = '100' src='../images/{$post_image}'></td>"; 
                           echo "<td>{$post_tags}</td>"; 
-                          echo "<td>{$post_comment_count}</td>"; 
+
+                          $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+                          $send_query_count = mysqli_query($connection, $query);
+                          $count_comments = mysqli_num_rows($send_query_count);
+
+
+                          echo "<td>{$count_comments}</td>"; 
+
+
                           echo "<td>{$post_date}</td>"; 
                           echo "<td><a href='../post.php?p_id={$post_id}'>VIEW POST</a></td>";
                           echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>EDIT</a></td>";
