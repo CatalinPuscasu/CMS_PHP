147dd47d338117320acfd7a61858bc75d73f69fc
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-03-31 21:36:26 +0300

    upgraded the header on the email function

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ if (isset($_POST['Send'])) {
   $to = "puscasucatalin437@gmail.com";
   $subject = wordwrap($_POST['subject'], 70);
   $body = $_POST['body'];
-  $header = $_POST['email'];
+  $header = "From: " . $_POST['email'];
 
 mail($to,$subject,$body, $header);
 
